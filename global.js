2fb216869ea51ae51a9c7a566b758223c9b570ce
Andrea <141286739+andiigonzalez@users.noreply.github.com>
2025-01-22 13:36:18 -0800

    Update global.js

--- global.js
+++ global.js
@@ -52,3 +52,17 @@ for (let p of pages) {
   li.appendChild(a);
   ul.appendChild(li);
 }
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
