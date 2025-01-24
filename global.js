d94c6bc02550759306ecbd4ffafd8979fb79b7a8
Andrea <141286739+andiigonzalez@users.noreply.github.com>
2025-01-24 14:24:26 -0800

    Update global.js

--- global.js
+++ global.js
@@ -10,10 +10,10 @@ const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 
 const pages = [
-  { url: "", title: "Home" },
-  { url: "projects/", title: "Projects" },
-  { url: "contact/", title: "Contact" },
-  { url: "portfolio/", title: "Resume" },
+  { url: "https://andiigonzalez.github.io/portfolio/index.html", title: "Home" },
+  { url: "https://andiigonzalez.github.io/portfolio/projects/index.html", title: "Projects" },
+  { url: "https://andiigonzalez.github.io/portfolio/contact/index.html", title: "Contact" },
+  { url: "https://andiigonzalez.github.io/portfolio/resume.html", title: "Resume" },
   { url: "https://github.com/andiigonzalez", title: "GitHub", external: true },
 ];
 const BASE_URL = "/portfolio/";
