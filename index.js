fd6c4f26352d9ecae441d62d16e88f272851afed
andiigonzalez <141286739+andiigonzalez@users.noreply.github.com>
2025-01-30 10:03:02 -0800

    added github stats

--- index.js
+++ index.js
@@ -7,4 +7,23 @@ const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 
-renderProjects(latestProjects, projectsContainer, 'h2');
+renderProjects(latestProjects, projectsContainer, 'h3');
+
+import { fetchGitHubData } from './global.js'; // Import function
+
+
+const githubData = await fetchGitHubData('giorgianicolaou'); // Fetch data
+const profileStats = document.querySelector('#profile-stats'); // Select HTML container
+
+if (profileStats) {
+    profileStats.innerHTML = `
+        <h3>My GitHub Statistics</h3>
+            <dl>
+                <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+                <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+                <dt>Followers:</dt><dd>${githubData.followers}</dd>
+                <dt>Following:</dt><dd>${githubData.following}</dd>
+            </dl>
+        `;
+    }
+
